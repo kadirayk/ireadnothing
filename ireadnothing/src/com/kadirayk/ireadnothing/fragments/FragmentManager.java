f20f4f5be83473b4a3d783db5e3cfb47b7a8ac07
Kadiray Karakaya <kadiray.k@hotmail.com>
2015-03-05 23:17:01 +0200

    we have the blank page up and running

---
+++ ireadnothing/src/com/kadirayk/ireadnothing/fragments/FragmentManager.java
@@ -0,0 +1,11 @@
+package com.kadirayk.ireadnothing.fragments;
+
+public class FragmentManager {
+
+	public interface FragmentTransactions{
+		public void onYMLEFragmentFired();
+		public void onYMLEDetailFragmentFired();
+		public void onYMLEArchivedFragmentFired();
+	}
+	
+}
